4fbacc02925ffa2e08ceb36d7040e9d1acb73d43
Chris Barrett <chris@codesaru.com>
2014-04-05 15:36:18 -0700

    First-pass chorded output.
    No mode shifting, and need to improve "awareness" of what's going on
    with pressing-- and especially releasing-- keys.  Works as a first test.

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,65 @@
 
 static const unsigned MS_PER_SECOND = 1000;
 
+// http://www.psdevwiki.com/ps4/DS4-USB
+enum EDs4Byte {
+    DS4_BYTE_REPORT_ID = 0,
+    DS4_BYTE_L_STICK_X_AXIS, // 0 = left
+    DS4_BYTE_L_STICK_Y_AXIS, // 0 = up
+    DS4_BYTE_R_STICK_X_AXIS,
+    DS4_BYTE_R_STICK_Y_AXIS,
+    DS4_BYTE_FACE_AND_POV,   // triangle | circle | x | square | 4-bit POV (1000b no pov)
+                             // 0111b NW, 0110b W, 0101b SW, 0100b S, 0011b SE, 0010b E, 0001b NE, 0000b N
+    DS4_BYTE_L_R_MISC_DIGITAL, // R3 | L3 | OPTIONS | SHARE | R2 | L2 | R1 | L1
+    DS4_BYTE_COUNTER_ETC,    // 6-bit counter (1 per report) | T-PAD click | guide button
+    DS4_BYTE_L2_ANALOG,      // 0 = released, 0xFF = fully pressed
+    DS4_BYTE_R2_ANALOG,
+    DS4_BYTE_BYTE_10,
+    DS4_BYTE_BYTE_11,
+    DS4_BYTE_BATTERY_LEVEL,
+    DS4_BYTE_13,
+    DS4_BYTE_14,
+    DS4_BYTE_15,
+    DS4_BYTE_16,
+    DS4_BYTE_17,
+    DS4_BYTE_18,
+    DS4_BYTE_19,
+    DS4_BYTE_20,
+    DS4_BYTE_21,
+    DS4_BYTE_22,
+    DS4_BYTE_23,
+    DS4_BYTE_24,
+    DS4_BYTE_25,
+    DS4_BYTE_26,
+    DS4_BYTE_27,
+    DS4_BYTE_28,
+    DS4_BYTE_29,
+    DS4_BYTE_30,
+    DS4_BYTE_31,
+    DS4_BYTE_32,
+    DS4_BYTE_33, // Some touchpad data starts here
+    DS4_BYTE_34,
+    DS4_BYTE_35,
+    DS4_BYTE_36,
+    DS4_BYTE_37,
+    DS4_BYTE_38,
+    DS4_BYTE_39,
+    DS4_BYTE_40,
+    DS4_BYTE_41,
+    DS4_BYTE_42,
+    DS4_BYTE_43,
+    DS4_BYTE_44,
+    DS4_BYTE_45,
+    DS4_BYTE_46,
+    DS4_BYTE_47,
+    DS4_BYTE_48,
+    DS4_BYTE_49,
+    DS4_BYTE_50,
+    DS4_BYTE_51,
+    // TODO : Remaining bytes
+    DS4_BYTES = 64
+};
+
 struct Ds4Frame {
     BYTE rawData[64];
 };
@@ -24,19 +83,45 @@ enum EGkosKeyFlags {
     GKOS_KEY_FLAG_ROW_BOT   = GKOS_KEY_FLAG_3 | GKOS_KEY_FLAG_6,
 };
 
-struct GkosEntry {
+struct GkosChord {
     const TCHAR *  str;
     unsigned short vkey;
 };
 
-static const unsigned s_frameDelay            = 4;
-static const unsigned s_frameHoldTime         = MS_PER_SECOND * 3;
-static const unsigned s_inputBufferCount      = s_frameHoldTime / s_frameDelay;
-static unsigned       s_inputBufferIndex      = 0;
-static Ds4Frame       s_ds4InputBuffer[s_inputBufferCount];
-static GkosEntry      s_chordInputBuffer[s_inputBufferCount];
+enum EGkosChordFlags {
+    GKOS_CHORD_FLAG_NONE       = 0,
+    GKOS_CHORD_FLAG_SHIFT      = 1 << 0,
+    GKOS_CHORD_FLAG_SYMB       = 1 << 1,
+    GKOS_CHORD_FLAG_SHIFT_LOCK = 1 << 2,
+    GKOS_CHORD_FLAG_SYMB_LOCK  = 1 << 3,
+    //GKOS_CHORD_FLAG_ = 1 << 4,
+    //GKOS_CHORD_FLAG_ = 1 << 5,
+    //GKOS_CHORD_FLAG_ = 1 << 6,
+    //GKOS_CHORD_FLAG_ = 1 << 7,
+    GKOS_CHORD_FLAGS_MASK = 0x0F
+};
 
-GkosEntry gkosKeysAbc[] = {
+struct GkosChordFrame {
+    BYTE     chordCode;
+    BYTE     flags;
+};
+
+// Timing
+static const unsigned s_ds4FrameDelay         = 4; // DS4 frame rate
+static const unsigned s_chordMinFrameCount    = 90 / s_ds4FrameDelay; // ms / ms-per-frame
+
+// Input frames
+static const unsigned s_inputBufferCount      = (MS_PER_SECOND * 3) / s_ds4FrameDelay;
+static unsigned       s_inputBufferIndex      = 0;
+static Ds4Frame       s_ds4FrameBuffer[s_inputBufferCount];
+static GkosChordFrame s_gkosFrameBuffer[s_inputBufferCount];
+
+// Higher-level key events
+//static const unsigned s_eventBufferCount      = 64; // Arbitrary number
+//static unsigned       s_eventBufferIndex      = 0;
+
+
+GkosChord gkosKeysAbc[] = {
     { NULL, 0 }, // Meaningless no-key placeholder
     { L"a", 0 }, // 1
     { L"b", 0 }, // 2
@@ -102,7 +187,7 @@ GkosEntry gkosKeysAbc[] = {
     { NULL, VK_DELETE },
     { NULL, 0 }, // TODO : ABC-123 toggle
 };
-GkosEntry gkosKeysSymb[] = {
+GkosChord gkosKeysSymb[] = {
     { NULL, 0 }, // Meaningless no-key placeholder
     { L"1", 0 }, // 1
     { L"2", 0 }, // 2
@@ -170,6 +255,16 @@ GkosEntry gkosKeysSymb[] = {
 };
 
 //=============================================================================
+static unsigned GetRelativeInputBufferIndex (unsigned framesAgo) {
+    return ((s_inputBufferIndex + s_inputBufferCount) - framesAgo) % s_inputBufferCount;
+}
+
+//=============================================================================
+static const GkosChordFrame & GetRelativeGkosChordFrame (unsigned framesAgo) {
+    return s_gkosFrameBuffer[GetRelativeInputBufferIndex(framesAgo)];
+}
+
+//=============================================================================
 void ListDevices () {
 
     UINT                numDevices;
@@ -220,65 +315,12 @@ void ReadDs4RawInput (
     unsigned     rawDataBytesEach,
     const BYTE * rawDataArray
 ) {
-    // http://www.psdevwiki.com/ps4/DS4-USB
-    enum EDs4Byte {
-        DS4_BYTE_REPORT_ID = 0,
-        DS4_BYTE_L_STICK_X_AXIS, // 0 = left
-        DS4_BYTE_L_STICK_Y_AXIS, // 0 = up
-        DS4_BYTE_R_STICK_X_AXIS,
-        DS4_BYTE_R_STICK_Y_AXIS,
-        DS4_BYTE_FACE_AND_POV,   // triangle | circle | x | square | 4-bit POV (1000b no pov)
-                                 // 0111b NW, 0110b W, 0101b SW, 0100b S, 0011b SE, 0010b E, 0001b NE, 0000b N
-        DS4_BYTE_L_R_MISC_DIGITAL, // R3 | L3 | OPTIONS | SHARE | R2 | L2 | R1 | L1
-        DS4_BYTE_COUNTER_ETC,    // 6-bit counter (1 per report) | T-PAD click | guide button
-        DS4_BYTE_L2_ANALOG,      // 0 = released, 0xFF = fully pressed
-        DS4_BYTE_R2_ANALOG,
-        DS4_BYTE_BYTE_10,
-        DS4_BYTE_BYTE_11,
-        DS4_BYTE_BATTERY_LEVEL,
-        DS4_BYTE_13,
-        DS4_BYTE_14,
-        DS4_BYTE_15,
-        DS4_BYTE_16,
-        DS4_BYTE_17,
-        DS4_BYTE_18,
-        DS4_BYTE_19,
-        DS4_BYTE_20,
-        DS4_BYTE_21,
-        DS4_BYTE_22,
-        DS4_BYTE_23,
-        DS4_BYTE_24,
-        DS4_BYTE_25,
-        DS4_BYTE_26,
-        DS4_BYTE_27,
-        DS4_BYTE_28,
-        DS4_BYTE_29,
-        DS4_BYTE_30,
-        DS4_BYTE_31,
-        DS4_BYTE_32,
-        DS4_BYTE_33, // Some touchpad data starts here
-        DS4_BYTE_34,
-        DS4_BYTE_35,
-        DS4_BYTE_36,
-        DS4_BYTE_37,
-        DS4_BYTE_38,
-        DS4_BYTE_39,
-        DS4_BYTE_40,
-        DS4_BYTE_41,
-        DS4_BYTE_42,
-        DS4_BYTE_43,
-        DS4_BYTE_44,
-        DS4_BYTE_45,
-        DS4_BYTE_46,
-        DS4_BYTE_47,
-        DS4_BYTE_48,
-        DS4_BYTE_49,
-        DS4_BYTE_50,
-        DS4_BYTE_51,
-        // TODO : Remaining bytes
-        DS4_BYTES = 64
-    };
 
+    s_inputBufferIndex = (s_inputBufferIndex + 1) % s_inputBufferCount;
+
+    Ds4Frame & ds4Frame = s_ds4FrameBuffer[s_inputBufferIndex];
+    memcpy(&ds4Frame, rawDataArray, rawDataBytesEach * rawDataCount);
+    
     unsigned povValue = rawDataArray[DS4_BYTE_FACE_AND_POV] & 0x0F;
     unsigned gkosChord = 0x0;
     if (rawDataArray[DS4_BYTE_L_R_MISC_DIGITAL] & (1 << 0)) // L1
@@ -294,8 +336,24 @@ void ReadDs4RawInput (
     if (rawDataArray[DS4_BYTE_FACE_AND_POV] & (1 << 5)) // X
         gkosChord |= (1 << 5);
         
-    if (gkosChord) {
-        const GkosEntry & gkosKey = gkosKeysAbc[gkosChord];
+    s_gkosFrameBuffer[s_inputBufferIndex].chordCode = gkosChord;
+    s_gkosFrameBuffer[s_inputBufferIndex].flags     = 0;
+    
+    if (!gkosChord)
+        return;
+
+    bool chordPressed = true;
+    for (unsigned i = 1; i < s_chordMinFrameCount; ++i) {
+        const GkosChordFrame & gkosFrame = GetRelativeGkosChordFrame(i);
+        if (gkosFrame.chordCode != gkosChord) {
+            chordPressed = false;
+            break;
+        }
+    }
+    
+    // Send the key if the chord was just pressed
+    if (chordPressed && GetRelativeGkosChordFrame(s_chordMinFrameCount).chordCode != gkosChord) {
+        const GkosChord & gkosKey = gkosKeysAbc[gkosChord];
         TCHAR buf[64];
         if (gkosKeysAbc[gkosChord].str)
             swprintf_s(buf, L"Chord 0x%02X yields Key %s\n", gkosChord, gkosKey.str);
@@ -345,18 +403,6 @@ LRESULT CALLBACK WndProc (
         case WM_PAINT: {
             hDC = BeginPaint(hwnd, &paint_struct);
             EndPaint(hwnd, &paint_struct);
-            return 0;
-        }
-        
-        case WM_CREATE: {
-            static RAWINPUTDEVICE rid[1];
-            rid[0].usUsagePage = 0x01; 
-            rid[0].usUsage     = 0x05; // Game pads (not joysticks) 
-            rid[0].dwFlags     = RIDEV_INPUTSINK;
-            rid[0].hwndTarget  = hwnd;
-
-            if (RegisterRawInputDevices(rid, sizeof(rid)/sizeof(rid[0]), sizeof(rid[0])) == FALSE)
-                return -1;
             return 0;
         }
 
@@ -422,16 +468,17 @@ int WINAPI wWinMain (
     int       command_show
 ) {
 
-    memset(s_ds4InputBuffer, 0, sizeof(s_ds4InputBuffer));
+    memset(s_ds4FrameBuffer, 0, sizeof(s_ds4FrameBuffer));
+    memset(s_gkosFrameBuffer, 0, sizeof(s_gkosFrameBuffer));
 
-    WNDCLASSEX windowClass = {0};
-    windowClass.cbSize = sizeof(windowClass);
-    windowClass.style = CS_HREDRAW | CS_VREDRAW;
-    windowClass.lpfnWndProc = WndProc;
-    windowClass.hInstance = instance;
-    windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    WNDCLASSEX windowClass    = {0};
+    windowClass.cbSize        = sizeof(windowClass);
+    windowClass.style         = CS_HREDRAW | CS_VREDRAW;
+    windowClass.lpfnWndProc   = WndProc;
+    windowClass.hInstance     = instance;
+    windowClass.hCursor       = LoadCursor(NULL, IDC_ARROW);
     windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    windowClass.lpszMenuName = NULL;
+    windowClass.lpszMenuName  = NULL;
     windowClass.lpszClassName = L"ps4gkos";
     
     if (!RegisterClassEx(&windowClass))
@@ -443,12 +490,22 @@ int WINAPI wWinMain (
     HWND hwnd = CreateWindowA("ps4gkos", "gkos Window", WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT,
         rc.right - rc.left, rc.bottom - rc.top,
-        NULL, NULL, instance, NULL);
+        NULL, NULL, instance, NULL
+    );
 
     if (!hwnd)
         return 1;
 
     ShowWindow(hwnd, command_show);
+    
+    static RAWINPUTDEVICE rid[1];
+    rid[0].usUsagePage = 0x01; 
+    rid[0].usUsage     = 0x05; // Game pads (not joysticks) 
+    rid[0].dwFlags     = RIDEV_INPUTSINK;
+    rid[0].hwndTarget  = hwnd;
+
+    if (RegisterRawInputDevices(rid, sizeof(rid)/sizeof(rid[0]), sizeof(rid[0])) == FALSE)
+        return 1;
 
     //GameTimer timer;
     //timer.Reset();
